6c412276042687298f09739497486ddc4cf07c5e
yiyizym <yiyizym@gmail.com>
2019-09-25 08:22:57 +0800

    feat: hide complete digits

--- src/components/digitPanel/index.tsx
+++ src/components/digitPanel/index.tsx
@@ -12,15 +12,17 @@ class DigitPanel extends React.Component<DigitPanelProps,{}> {
   private generateDigits(): JSX.Element[] {
     const { toFillCount, selectDigit, digitChosen } = this.props
     const digitCounts = Object.entries(toFillCount)
-    return digitCounts.map(([digit, count]) => {
-      return <div
-        key={digit}
-        className={`digit-panel-item ${digit === digitChosen ? 'selected' : ''}`}
-        onClick={() => selectDigit(digit)}
-      >
-        {count ? Array.from({ length: count }, ():string => digit).join(' ') : null}
-      </div>
-    })
+    return digitCounts
+      .filter(([digit, count]) => !!count)
+      .map(([digit, count]) => {
+        return <div
+          key={digit}
+          className={`digit-panel-item ${digit === digitChosen ? 'selected' : ''}`}
+          onClick={() => selectDigit(digit)}
+        >
+          {Array.from({ length: count }, ():string => digit).join(' ')}
+        </div>
+      })
   }
 
   public render(): JSX.Element {
