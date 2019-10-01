b39b53e34f72e2dc22d1373351a81fa1f3f378b5
yiyizym <yiyizym@gmail.com>
2019-10-01 11:41:59 +0800

    feat: ensure digit default chosen

--- src/components/digitPanel/index.tsx
+++ src/components/digitPanel/index.tsx
@@ -3,17 +3,25 @@ import "./index.scss";
 
 interface DigitPanelProps {
   toFillCount: { [key: string]: number};
-  selectDigit: (digit: string) => void;
   digitChosen: string;
+  selectDigit: (digit: string) => void;
 }
 
 class DigitPanel extends React.Component<DigitPanelProps,{}> {
+
+  public componentDidMount(): void {
+    this.ensureDigitChosen()
+  }
+
+  public componentDidUpdate(): void {
+    this.ensureDigitChosen()
+  }
 
   private generateDigits(): JSX.Element[] {
     const { toFillCount, selectDigit, digitChosen } = this.props
     const digitCounts = Object.entries(toFillCount)
     return digitCounts
-      .filter(([digit, count]) => !!count)
+      .filter(([, count]) => !!count)
       .map(([digit, count]) => {
         return <div
           key={digit}
@@ -23,6 +31,21 @@ class DigitPanel extends React.Component<DigitPanelProps,{}> {
           {Array.from({ length: count }, ():string => digit).join(' ')}
         </div>
       })
+  }
+
+  private ensureDigitChosen = (): void => {
+    const { digitChosen, toFillCount, selectDigit } = this.props
+    if(digitChosen && toFillCount[digitChosen]) { return }
+    let chosenDigit = '0', currentCount = 10
+    const digitCounts = Object.entries(toFillCount)
+    for (let [key, val] of digitCounts) {
+      if (chosenDigit === '0' || (val !== 0 && val < currentCount)) {
+        chosenDigit = key
+        currentCount = val
+      }
+    }
+    if(chosenDigit === '0') { return } // we finished the game
+    selectDigit(chosenDigit)
   }
 
   public render(): JSX.Element {
